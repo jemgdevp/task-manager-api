11105fbae7ea3a4b9f70e486403c4c090d73395b
Juan Esteban Manrique Giraldo <murksopps@gmail.com>
2026-03-27 17:10:16 -0500

    fix: comment out migration command in deployment script

--- .docker/deploy.sh
+++ .docker/deploy.sh
@@ -31,7 +31,7 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Ejecutar migraciones
-php artisan migrate --seed --force
+# Ejecutar migraciones (manual)
+#php artisan migrate --seed --force
 
 echo "Deploy completed successfully"
